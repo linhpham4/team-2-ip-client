6619ed6504e44eaf6b5fae1d2c321afa96a80ac5
Angelo <yapangeloterezo@gmail.com>
2024-09-13 00:13:50 -0700

    ignore commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Link>
+    <Link to="/">
       <img src={header} className="header__image" />
     </Link>
   );
--- src/pages/FeedBackPage/FeedBackPage.jsx
+++ src/pages/FeedBackPage/FeedBackPage.jsx
@@ -5,40 +5,42 @@ import input from "../../assets/images/input.svg";
 
 const FeedBackPage = () => {
   return (
-    <div className="feedbackpage">
-      <div className="feedbackpage__hero-container">
-        <img src={hero} alt="hero" className="feedbackpage__hero" />
-        <img src={tabs} alt="tabs" className="feedbackpage__tabs" />
-      </div>
-      <div className="feedbackpage__section-header-container">
-        <h1 className="feedbackpage__section-header">Reviews</h1>
-        <img src={input} alt="input" className="feedbackpage__search" />
-      </div>
-      <div className="feedbackpage__review-container">
-        <div className="feedbackpage__review-container-top">
-          <div className="feedbackpage__left-details">
-            <h3 className="feedbackpage__review-title">
-              Strings are garbage but not a bad guitar
-            </h3>
-            <div className="feedbackpage__review-details">
-              <p className="feedbackpage__timestamp">
-                Reviewed in Canada on June 21, 2024
-              </p>
-              <p className="feedbackpage__verified">Verified Purchase</p>
+    <>
+      <div className="feedbackpage">
+        <div className="feedbackpage__hero-container">
+          <img src={hero} alt="hero" className="feedbackpage__hero" />
+          <img src={tabs} alt="tabs" className="feedbackpage__tabs" />
+        </div>
+        <div className="feedbackpage__section-header-container">
+          <h1 className="feedbackpage__section-header">Reviews</h1>
+          <img src={input} alt="input" className="feedbackpage__search" />
+        </div>
+        <div className="feedbackpage__review-container">
+          <div className="feedbackpage__review-container-top">
+            <div className="feedbackpage__left-details">
+              <h3 className="feedbackpage__review-title">
+                Strings are garbage but not a bad guitar
+              </h3>
+              <div className="feedbackpage__review-details">
+                <p className="feedbackpage__timestamp">
+                  Reviewed in Canada on June 21, 2024
+                </p>
+                <p className="feedbackpage__verified">Verified Purchase</p>
+              </div>
             </div>
+            <p className="feedbackpage__credit">$5.25 Amazon Credit</p>
           </div>
-          <p className="feedbackpage__credit">$5.25 Amazon Credit</p>
-        </div>
-        <div className="feedbackpage__review-container-bottom">
-          <p className="feedbackpage__review-container-preview">
-            The strings seemed cheap despite the advertisement that they were
-            from a quality string company. Watched a quick youtube video and
-            replaced the strings myself with better ones. All in all not a bad
-            little{" "}
-          </p>
+          <div className="feedbackpage__review-container-bottom">
+            <p className="feedbackpage__review-container-preview">
+              The strings seemed cheap despite the advertisement that they were
+              from a quality string company. Watched a quick youtube video and
+              replaced the strings myself with better ones. All in all not a bad
+              little{" "}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default FeedBackPage;
